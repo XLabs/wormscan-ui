8491a917dca3b6d1137b6c7c8c384e58d0eff8d7
Ricardo Olarte <raop155@hotmail.com>
2023-02-14 16:16:49 -0500

    feat: add Footer styling as draft design

--- src/components/molecules/Footer/index.tsx
+++ src/components/molecules/Footer/index.tsx
@@ -1,10 +1,43 @@
+import { NavLink } from "react-router-dom";
+import DiscordIcon from "src/icons/DiscordIcon";
+import TwitterIcon from "src/icons/TwitterIcon";
 import WormholeBrand from "../WormholeBrand";
+
+import "./styles.scss";
 
 const Footer = () => {
   return (
-    <div>
+    <footer className="footer">
       <WormholeBrand width={49} height={43} />
-    </div>
+
+      <div className="footer-links">
+        <div className="footer-links-item">
+          <NavLink to="/">FAQs</NavLink>
+        </div>
+
+        <div className="footer-links-item">
+          <NavLink to="/">Status</NavLink>
+        </div>
+
+        <div className="footer-links-item">
+          <NavLink to="/">Bridge</NavLink>
+        </div>
+
+        <div className="footer-links-item">
+          <NavLink to="/">Stats</NavLink>
+        </div>
+
+        <div className="footer-links-item">
+          <NavLink to="/">Contact us</NavLink>
+        </div>
+      </div>
+
+      <div className="footer-social">
+        Join us:
+        <DiscordIcon />
+        <TwitterIcon />
+      </div>
+    </footer>
   );
 };
 
