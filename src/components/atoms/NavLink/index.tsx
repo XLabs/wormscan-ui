887b3c810a314cbc153a1fbaa8b40fa34377dd1a
Ricardo Olarte <raop155@hotmail.com>
2023-04-12 14:25:40 -0500

    fix: footer styling mobile and desktop

--- src/components/atoms/NavLink/index.tsx
+++ src/components/atoms/NavLink/index.tsx
@@ -1,10 +1,11 @@
 import { NavLink as RealNavLink, NavLinkProps } from "react-router-dom";
+import "./style.scss";
 
 const NavLink = (props: NavLinkProps) => {
   const goTop = () => window.scrollTo(0, 0);
 
   return (
-    <RealNavLink {...props} onClick={goTop}>
+    <RealNavLink className="navlink" {...props} onClick={goTop}>
       {props.children}
     </RealNavLink>
   );
