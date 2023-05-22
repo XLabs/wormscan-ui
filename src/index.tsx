794b0faebb42210c107199e73ed59e663b9a5588
Ricardo Olarte <raop155@hotmail.com>
2023-05-22 14:50:40 -0500

    fix: remove window.devicePixelRatio

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.scss";
 import "./styles/globals.scss";
 import "./i18n";
 import App from "./App";
-window.devicePixelRatio = 1; // To handle poor performance on high resolution screens
 
 const root = createRoot(document.getElementById("app")!);
 root.render(<App />);
