dfc5e7db63e4269ff553c295fcc2cf1bf286a290
Ricardo Olarte <raop155@hotmail.com>
2023-05-22 12:41:21 -0500

    fix(74): scorecard mobile, performace with bg images and pixel ratio

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.scss";
 import "./styles/globals.scss";
 import "./i18n";
 import App from "./App";
+window.devicePixelRatio = 1; // To handle poor performance on high resolution screens
 
 const root = createRoot(document.getElementById("app")!);
 root.render(<App />);
