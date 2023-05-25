5d7042e9afc12b9738bd614e1c578be522cbf1c9
Ricardo Olarte <raop155@hotmail.com>
2023-05-25 17:22:05 -0500

    feat: add Tag component

--- src/pages/Tx/Top/index.tsx
+++ src/pages/Tx/Top/index.tsx
@@ -1,6 +1,7 @@
 import { CopyIcon } from "@radix-ui/react-icons";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
+import Tag from "src/components/atoms/Tag";
 import CopyToClipboard from "src/components/molecules/CopyToClipboard";
 import "./styles.scss";
 
@@ -12,6 +13,7 @@ const Top = () => {
     <section className="tx-top">
       <div className="tx-top-header">
         <h1 className="tx-top-header-title">{t("tx.top.title")}</h1>
+        <Tag className="blue">Transfer</Tag>
       </div>
       <div className="tx-top-txId">
         HASH: {txHash}
