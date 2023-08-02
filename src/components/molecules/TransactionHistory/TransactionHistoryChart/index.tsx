3f0e2473ea7c86625a73248191fbc2c7ba1c5b1b
Giuliano Conti <giuliconti1@gmail.com>
2023-08-02 19:34:48 -0300

    Implement new transaction history design (#253)
    * Add mock for 3 months on transaction history
    
    * New design for transaction history, more information added
    
    * data3Months inline
    
    * Graph with more space on the left in 1200 or less

--- src/components/molecules/TransactionHistory/TransactionHistoryChart/index.tsx
+++ src/components/molecules/TransactionHistory/TransactionHistoryChart/index.tsx
@@ -6,17 +6,26 @@ import { DateRange } from "@xlabs-libs/wormscan-sdk";
 import { getClient } from "src/api/Client";
 import { numberToSuffix } from "src/utils/number";
 import { ErrorPlaceholder } from "src/components/molecules";
+import { useWindowSize } from "src/utils/hooks/useWindowSize";
 import "./styles.scss";
 
 type Props = {
   range: DateRange;
 };
 
+// TODO: Remove this mock data when real data is available
+// prettier-ignore
+const data3Months = [ { time: "2023-07-31T20:26:20.505228728Z", count: 2981, }, { time: "2023-07-30T00:00:00Z", count: 1276, }, { time: "2023-07-29T00:00:00Z", count: 1214, }, { time: "2023-07-28T00:00:00Z", count: 979, }, { time: "2023-07-27T00:00:00Z", count: 943, }, { time: "2023-07-26T00:00:00Z", count: 1282, }, { time: "2023-07-25T00:00:00Z", count: 925, }, { time: "2023-07-24T00:00:00Z", count: 1132, }, { time: "2023-07-23T00:00:00Z", count: 1079, }, { time: "2023-07-22T00:00:00Z", count: 1073, }, { time: "2023-07-21T00:00:00Z", count: 964, }, { time: "2023-07-20T00:00:00Z", count: 962, }, { time: "2023-07-19T00:00:00Z", count: 1010, }, { time: "2023-07-18T00:00:00Z", count: 1123, }, { time: "2023-07-17T00:00:00Z", count: 1215, }, { time: "2023-07-16T00:00:00Z", count: 965, }, { time: "2023-07-15T00:00:00Z", count: 965, }, { time: "2023-07-14T00:00:00Z", count: 1189, }, { time: "2023-07-13T00:00:00Z", count: 1050, }, { time: "2023-07-12T00:00:00Z", count: 1239, }, { time: "2023-07-11T00:00:00Z", count: 1304, }, { time: "2023-07-10T00:00:00Z", count: 2004, }, { time: "2023-07-09T00:00:00Z", count: 1246, }, { time: "2023-07-08T00:00:00Z", count: 1130, }, { time: "2023-07-07T00:00:00Z", count: 1136, }, { time: "2023-07-06T00:00:00Z", count: 1165, }, { time: "2023-07-05T00:00:00Z", count: 1000, }, { time: "2023-07-04T00:00:00Z", count: 1126, }, { time: "2023-07-03T00:00:00Z", count: 1279, }, { time: "2023-07-02T00:00:00Z", count: 1122, }, { time: "2023-07-01T00:00:00Z", count: 88, }, { time: "2023-06-31T20:26:20.505228728Z", count: 2981, }, { time: "2023-06-30T00:00:00Z", count: 1276, }, { time: "2023-06-29T00:00:00Z", count: 1214, }, { time: "2023-06-28T00:00:00Z", count: 979, }, { time: "2023-06-27T00:00:00Z", count: 943, }, { time: "2023-06-26T00:00:00Z", count: 1282, }, { time: "2023-06-25T00:00:00Z", count: 925, }, { time: "2023-06-24T00:00:00Z", count: 1132, }, { time: "2023-06-23T00:00:00Z", count: 1079, }, { time: "2023-06-22T00:00:00Z", count: 1073, }, { time: "2023-06-21T00:00:00Z", count: 964, }, { time: "2023-06-20T00:00:00Z", count: 962, }, { time: "2023-06-19T00:00:00Z", count: 1010, }, { time: "2023-06-18T00:00:00Z", count: 1123, }, { time: "2023-06-17T00:00:00Z", count: 1215, }, { time: "2023-06-16T00:00:00Z", count: 965, }, { time: "2023-06-15T00:00:00Z", count: 965, }, { time: "2023-06-14T00:00:00Z", count: 1189, }, { time: "2023-06-13T00:00:00Z", count: 1050, }, { time: "2023-06-12T00:00:00Z", count: 1239, }, { time: "2023-06-11T00:00:00Z", count: 1304, }, { time: "2023-06-10T00:00:00Z", count: 2004, }, { time: "2023-06-09T00:00:00Z", count: 1246, }, { time: "2023-06-08T00:00:00Z", count: 1130, }, { time: "2023-06-07T00:00:00Z", count: 1136, }, { time: "2023-06-06T00:00:00Z", count: 1165, }, { time: "2023-06-05T00:00:00Z", count: 1000, }, { time: "2023-06-04T00:00:00Z", count: 1126, }, { time: "2023-06-03T00:00:00Z", count: 1279, }, { time: "2023-06-02T00:00:00Z", count: 1122, }, { time: "2023-06-01T00:00:00Z", count: 88, }, { time: "2023-05-31T20:26:20.505228728Z", count: 2981, }, { time: "2023-05-30T00:00:00Z", count: 1276, }, { time: "2023-05-29T00:00:00Z", count: 1214, }, { time: "2023-05-28T00:00:00Z", count: 979, }, { time: "2023-05-27T00:00:00Z", count: 943, }, { time: "2023-05-26T00:00:00Z", count: 1282, }, { time: "2023-05-25T00:00:00Z", count: 925, }, { time: "2023-05-24T00:00:00Z", count: 1132, }, { time: "2023-05-23T00:00:00Z", count: 1079, }, { time: "2023-05-22T00:00:00Z", count: 1073, }, { time: "2023-05-21T00:00:00Z", count: 964, }, { time: "2023-05-20T00:00:00Z", count: 962, }, { time: "2023-05-19T00:00:00Z", count: 1010, }, { time: "2023-05-18T00:00:00Z", count: 1123, }, { time: "2023-05-17T00:00:00Z", count: 1215, }, { time: "2023-05-16T00:00:00Z", count: 965, }, { time: "2023-05-15T00:00:00Z", count: 965, }, { time: "2023-05-14T00:00:00Z", count: 1189, }, { time: "2023-05-13T00:00:00Z", count: 1050, }, { time: "2023-05-12T00:00:00Z", count: 1239, }, { time: "2023-05-11T00:00:00Z", count: 1304, }, { time: "2023-05-10T00:00:00Z", count: 2004, }, { time: "2023-05-09T00:00:00Z", count: 1246, }, { time: "2023-05-08T00:00:00Z", count: 1130, }, { time: "2023-05-07T00:00:00Z", count: 1136, }, { time: "2023-05-06T00:00:00Z", count: 1165, }, { time: "2023-05-05T00:00:00Z", count: 1000, }, { time: "2023-05-04T00:00:00Z", count: 1126, }, { time: "2023-05-03T00:00:00Z", count: 1279, }, { time: "2023-05-02T00:00:00Z", count: 1122, }, { time: "2023-05-01T00:00:00Z", count: 88, },
+];
+
 const TransactionHistoryChart = ({ range }: Props) => {
   const [seriesData, setSeriesData] = useState([0]);
   const [seriesLabels, setSeriesLabels] = useState([""]);
   const [totalTxs, setTotalTxs] = useState("");
-  const tickAmount = range === "month" ? 4 : 5;
+  const tickAmount = range === "3-month" ? 3 : range === "month" ? 4 : 5;
+  const size = useWindowSize();
+  const isWidthOver1200px = size.width >= 1200;
+  const [dataReverse, setDataReverse] = useState([]);
 
   const { data, isError, isLoading, isFetching } = useQuery(
     ["getLastTxs", range],
@@ -25,12 +34,19 @@ const TransactionHistoryChart = ({ range }: Props) => {
   );
 
   useEffect(() => {
-    if (!data) return;
+    // TODO delete range !== "3-month" when api is ready
+    if (!data && range !== "3-month") return;
 
-    const responseReversed = data.reverse();
+    // TODO delete data3Months when api is ready
+    const responseReversed = range !== "3-month" ? [...data].reverse() : [...data3Months].reverse();
     const totalAmount = responseReversed.reduce((prev, curr) => prev + curr.count, 0);
 
-    setTotalTxs(`Last ${range === "day" ? "24hs" : range}: ${totalAmount.toLocaleString()} txs`);
+    setDataReverse(responseReversed);
+    setTotalTxs(
+      `Last ${
+        range === "day" ? "24hs" : range === "3-month" ? "3 months" : range
+      }: ${totalAmount.toLocaleString()} txs`,
+    );
     setSeriesData(responseReversed.map(item => item.count));
     setSeriesLabels(
       responseReversed.map(item => {
@@ -51,9 +67,20 @@ const TransactionHistoryChart = ({ range }: Props) => {
         if (range === "month") {
           return date.toLocaleString("en", { month: "short", day: "numeric" });
         }
+
+        if (range === "3-month") {
+          return date
+            .toLocaleString("en", {
+              month: "short",
+              day: "numeric",
+              year: isWidthOver1200px ? "numeric" : "2-digit",
+            })
+            .replace(", ", isWidthOver1200px ? " / " : "/")
+            .toUpperCase();
+        }
       }),
     );
-  }, [data, range]);
+  }, [data, range, isWidthOver1200px]);
 
   return (
     <div className="trans-history" data-range={range}>
@@ -61,7 +88,8 @@ const TransactionHistoryChart = ({ range }: Props) => {
         <Loader />
       ) : (
         <>
-          {isError ? (
+          {/* TODO delete range !== "3-month" when api is ready */}
+          {range !== "3-month" && isError ? (
             <ErrorPlaceholder errorType="chart" />
           ) : (
             <div className="trans-history-chart">
@@ -84,6 +112,7 @@ const TransactionHistoryChart = ({ range }: Props) => {
                       fontSize: "16px",
                       fontWeight: 400,
                     },
+                    offsetX: -8,
                   },
                   fill: {
                     type: "gradient",
@@ -117,8 +146,43 @@ const TransactionHistoryChart = ({ range }: Props) => {
                   },
                   grid: {
                     show: false,
+                    padding: {
+                      // This is to make the chart look aligned to the right
+                      right: isWidthOver1200px ? seriesLabels[0].length * -2.4 : 10,
+                      left: -2,
+                    },
                   },
                   tooltip: {
+                    custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+                      const index = w.globals.labels[dataPointIndex] - 1;
+
+                      const date = dataReverse[index].time;
+                      const transactions = series[seriesIndex][dataPointIndex];
+
+                      const parsedDate = new Date(date);
+                      const formattedDate = parsedDate.toLocaleString("en-US", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        hour12: true,
+                      });
+
+                      return `
+                        <div class="trans-history-chart-info">
+                          <p class="trans-history-chart-info-paragraph">#Transactions: ${transactions}</p>
+                          ${
+                            // TODO, uncomment when endpoint returns volume
+                            // <p class="trans-history-chart-info-paragraph">Volume: $8.24M</p>
+                            ""
+                          }
+                          <p class="trans-history-chart-info-paragraph">${formattedDate}</p>
+                          <span class="trans-history-chart-info-span">Hours correspond to current device's local time.</span>
+                         
+                        </div>
+                      `;
+                    },
                     enabled: true,
                     x: {
                       show: false,
@@ -140,6 +204,7 @@ const TransactionHistoryChart = ({ range }: Props) => {
                     tickAmount: 4,
                     labels: {
                       formatter: numberToSuffix,
+                      offsetX: -14,
                       style: {
                         colors: "var(--color-primary-50)",
                         fontFamily: "IBM Plex Sans",
@@ -154,31 +219,21 @@ const TransactionHistoryChart = ({ range }: Props) => {
                   },
                   xaxis: {
                     labels: {
-                      hideOverlappingLabels: false,
+                      offsetX: 0,
                       style: {
                         colors: "var(--color-primary-50)",
                         fontFamily: "IBM Plex Sans",
                         fontSize: "14px",
                       },
-                      rotate: 0,
+                      rotate: isWidthOver1200px ? 0 : -45,
+                      rotateAlways: !isWidthOver1200px,
                     },
                     tickAmount,
                     tickPlacement: "on",
                     axisTicks: { show: false },
                     axisBorder: { show: true, strokeWidth: 4, color: "#FFFFFF25" },
+                    tooltip: { enabled: false },
                   },
-                  responsive: [
-                    {
-                      breakpoint: 1200,
-                      options: {
-                        xaxis: {
-                          labels: {
-                            rotate: -45,
-                          },
-                        },
-                      },
-                    },
-                  ],
                 }}
               />
             </div>
