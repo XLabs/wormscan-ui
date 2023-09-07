561d908711f4833e27f7bcff2729915ac13dc3e2
giulianoconti <giuliconti1@gmail.com>
2023-09-06 21:31:23 -0300

    changes in tx detail tabs,  when we are on mobile

--- src/pages/Tx/Information/Tabs/index.tsx
+++ src/pages/Tx/Information/Tabs/index.tsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
 import { ListBulletIcon } from "@radix-ui/react-icons";
-import { Tooltip } from "src/components/atoms";
+import { useWindowSize } from "src/utils/hooks/useWindowSize";
 import OverviewIcon from "src/icons/OverviewIcon";
+import { Tooltip } from "src/components/atoms";
+import { BREAKPOINTS } from "src/consts";
 import "./styles.scss";
 
 type TabsProps = {
@@ -17,62 +20,98 @@ const Tabs = ({
   setShowOverviewDetail,
   showOverview,
   showOverviewDetail,
-}: TabsProps) => (
-  <div className="tx-tabs">
-    <button
-      className={`tx-tabs-trigger ${showOverview ? "tx-tabs-trigger-active" : ""}`}
-      aria-label="Overview"
-      onClick={() => setShowOverview(true)}
-    >
-      OVERVIEW
-    </button>
-    <button
-      className={`tx-tabs-trigger ${!showOverview ? "tx-tabs-trigger-active" : ""}`}
-      aria-label="Raw data"
-      onClick={() => setShowOverview(false)}
-    >
-      RAW DATA
-    </button>
+}: TabsProps) => {
+  const [openTooltips, setOpenTooltips] = useState([false, false]);
+  const { width } = useWindowSize();
+  const isMobileOrTablet = width < BREAKPOINTS.desktop;
 
-    {showOverview && !isGenericRelayerTx && (
-      <div className="tx-tabs-sub-list">
-        <div className="tx-tabs-sub-list-title">Views</div>
+  useEffect(() => {
+    if (!openTooltips.includes(true)) return;
 
-        <div className="tx-tabs-sub-list-divider">
-          <Tooltip
-            tooltip={<div className="tx-tabs-sub-list-divider-tooltip">Overview</div>}
-            type="info"
+    const timeout = setTimeout(() => {
+      setOpenTooltips([false, false]);
+    }, 1500);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [openTooltips]);
+
+  const openTooltip = (index: number) => {
+    const newTooltips = [false, false];
+    newTooltips[index] = true;
+    setOpenTooltips(newTooltips);
+  };
+
+  const [openTooltip1, openTooltip2] = openTooltips;
+
+  return (
+    <div className="tx-tabs">
+      <button
+        className={`tx-tabs-trigger ${showOverview ? "tx-tabs-trigger-active" : ""}`}
+        aria-label="Overview"
+        onClick={() => setShowOverview(true)}
+      >
+        OVERVIEW
+      </button>
+      <button
+        className={`tx-tabs-trigger ${!showOverview ? "tx-tabs-trigger-active" : ""}`}
+        aria-label="Raw data"
+        onClick={() => setShowOverview(false)}
+      >
+        RAW DATA
+      </button>
+
+      {showOverview && !isGenericRelayerTx && (
+        <div className="tx-tabs-sub-list">
+          <div className="tx-tabs-sub-list-title">Views</div>
+
+          <div
+            className="tx-tabs-sub-list-divider"
+            onClick={isMobileOrTablet ? () => openTooltip(0) : undefined}
           >
-            <button
-              className={`tx-tabs-sub-list-divider-button ${
-                !showOverviewDetail ? "tx-tabs-sub-list-divider-button-active" : ""
-              }`}
-              aria-label="Overview"
-              onClick={() => setShowOverviewDetail(false)}
+            <Tooltip
+              tooltip={<div className="tx-tabs-sub-list-divider-tooltip">Overview</div>}
+              type="info"
+              open={openTooltip1}
+              controlled={isMobileOrTablet}
             >
-              <OverviewIcon height={24} width={24} />
-            </button>
-          </Tooltip>
-        </div>
-        <div className="tx-tabs-sub-list-divider">
-          <Tooltip
-            tooltip={<div className="tx-tabs-sub-list-divider-tooltip">Details</div>}
-            type="info"
+              <button
+                className={`tx-tabs-sub-list-divider-button ${
+                  !showOverviewDetail ? "tx-tabs-sub-list-divider-button-active" : ""
+                }`}
+                aria-label="Overview"
+                onClick={() => setShowOverviewDetail(false)}
+              >
+                <OverviewIcon height={24} width={24} />
+              </button>
+            </Tooltip>
+          </div>
+          <div
+            className="tx-tabs-sub-list-divider"
+            onClick={isMobileOrTablet ? () => openTooltip(1) : undefined}
           >
-            <button
-              className={`tx-tabs-sub-list-divider-button ${
-                showOverviewDetail ? "tx-tabs-sub-list-divider-button-active" : ""
-              }`}
-              aria-label="Details list"
-              onClick={() => setShowOverviewDetail(true)}
+            <Tooltip
+              tooltip={<div className="tx-tabs-sub-list-divider-tooltip">Details</div>}
+              type="info"
+              open={openTooltip2}
+              controlled={isMobileOrTablet}
             >
-              <ListBulletIcon height={24} width={24} />
-            </button>
-          </Tooltip>
+              <button
+                className={`tx-tabs-sub-list-divider-button ${
+                  showOverviewDetail ? "tx-tabs-sub-list-divider-button-active" : ""
+                }`}
+                aria-label="Details list"
+                onClick={() => setShowOverviewDetail(true)}
+              >
+                <ListBulletIcon height={24} width={24} />
+              </button>
+            </Tooltip>
+          </div>
         </div>
-      </div>
-    )}
-  </div>
-);
+      )}
+    </div>
+  );
+};
 
 export default Tabs;
