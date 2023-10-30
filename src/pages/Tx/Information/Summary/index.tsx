623488cf030d2dead811eb797478d590c0a622c1
giulianoconti <giuliconti1@gmail.com>
2023-10-30 15:54:28 -0300

    change to be able to deploy

--- src/pages/Tx/Information/Summary/index.tsx
+++ src/pages/Tx/Information/Summary/index.tsx
@@ -17,10 +17,10 @@ type Props = {
   vaa: string;
 };
 
-const StatusCompleted = () => (
-  <Chip className="status" color="completed">
-    <CheckCircledIcon height={16} width={16} />
-    COMPLETED
+const StatusInProgress = () => (
+  <Chip className="status" color="progress">
+    <ClockIcon height={16} width={16} />
+    IN PROGRESS
   </Chip>
 );
 
@@ -42,10 +42,22 @@ const StatusWaitingRedeem = () => (
   </Tooltip>
 );
 
-const StatusInProgress = () => (
-  <Chip className="status" color="progress">
-    <ClockIcon height={16} width={16} />
-    IN PROGRESS
+const StatusCompleted = () => (
+  <Chip className="status" color="completed">
+    <CheckCircledIcon height={16} width={16} />
+    COMPLETED
+  </Chip>
+);
+
+const StatusUnknown = () => (
+  <Chip className="status" color="unknown">
+    UNKNOWN
+  </Chip>
+);
+
+const StatusIndeterminate = () => (
+  <Chip className="status" color="indeterminate">
+    INDETERMINATE
   </Chip>
 );
 
@@ -58,68 +70,99 @@ const Summary = ({
   parsedDestinationAddress,
   toChain,
   vaa,
-}: Props) => (
-  <div className="tx-information-summary">
-    <div>
-      <div className="key">Status:</div>
-      <div className="value">
-        {vaa ? (
-          isCCTPConnectOrPortalApp && !isUnknownApp ? (
-            globalToRedeemTx ? (
-              <StatusCompleted />
+}: Props) => {
+  // contract-watcher:
+  // if toChain is aptos, arbitrium, avalanche, base, bsc, celo, ethereum, fantom,
+  // moonbeam, oasis, optimism, polygon, solana or terra we can get destinationTx.
+  const weCanGetDestinationTx = [
+    ChainId.Aptos,
+    ChainId.Arbitrum,
+    ChainId.Avalanche,
+    ChainId.Base,
+    ChainId.BSC,
+    ChainId.Celo,
+    ChainId.Ethereum,
+    ChainId.Fantom,
+    ChainId.Moonbeam,
+    ChainId.Oasis,
+    ChainId.Optimism,
+    ChainId.Polygon,
+    ChainId.Solana,
+    ChainId.Terra,
+  ].includes(toChain);
+
+  return (
+    <div className="tx-information-summary">
+      <div>
+        <div className="key">Status:</div>
+        <div className="value">
+          {/* TODO (waiting design)
+          
+          {vaa ? (
+            isUnknownApp || !(appIds?.length > 0) ? (
+              <StatusUnknown />
+            ) : isCCTPConnectOrPortalApp ? (
+              weCanGetDestinationTx ? (
+                globalToRedeemTx ? (
+                  <StatusCompleted />
+                ) : (
+                  <StatusWaitingRedeem />
+                )
+              ) : (
+                <StatusIndeterminate />
+              )
             ) : (
-              <StatusWaitingRedeem />
+              <StatusCompleted />
             )
           ) : (
-            <StatusCompleted />
-          )
-        ) : (
-          <StatusInProgress />
-        )}
+            <StatusInProgress />
+          )} */}
+          {vaa ? <StatusCompleted /> : <StatusInProgress />}
+        </div>
       </div>
-    </div>
-    <div>
-      <div className="key">Origin App:</div>
-      <div className="value">{appIds?.length > 0 ? formatAppIds(appIds) : "N/A"}</div>
-    </div>
-    <div>
-      <div className="key">Destination Wallet:</div>
-      <div className="value">
-        {parsedDestinationAddress ? (
-          <a
-            href={getExplorerLink({
-              network: currentNetwork,
-              chainId: toChain,
-              value: parsedDestinationAddress,
-              base: "address",
-              isNativeAddress: true,
-            })}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {shortAddress(parsedDestinationAddress).toUpperCase()}
-          </a>
-        ) : (
-          "N/A"
-        )}
-        {isUnknownApp && (
-          <div className="value-tooltip">
-            <Tooltip
-              tooltip={
-                <div>
-                  Address shown corresponds to a Smart Contract handling the transaction. Funds will
-                  be sent to your recipient address.
-                </div>
-              }
-              type="info"
+      <div>
+        <div className="key">Origin App:</div>
+        <div className="value">{appIds?.length > 0 ? formatAppIds(appIds) : "N/A"}</div>
+      </div>
+      <div>
+        <div className="key">Destination Wallet:</div>
+        <div className="value">
+          {parsedDestinationAddress ? (
+            <a
+              href={getExplorerLink({
+                network: currentNetwork,
+                chainId: toChain,
+                value: parsedDestinationAddress,
+                base: "address",
+                isNativeAddress: true,
+              })}
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <InfoCircledIcon />
-            </Tooltip>
-          </div>
-        )}
+              {shortAddress(parsedDestinationAddress).toUpperCase()}
+            </a>
+          ) : (
+            "N/A"
+          )}
+          {isUnknownApp && (
+            <div className="value-tooltip">
+              <Tooltip
+                tooltip={
+                  <div>
+                    Address shown corresponds to a Smart Contract handling the transaction. Funds
+                    will be sent to your recipient address.
+                  </div>
+                }
+                type="info"
+              >
+                <InfoCircledIcon />
+              </Tooltip>
+            </div>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Summary;
