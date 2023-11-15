1ee0b18c64f03b33fc5cbf8d3389b468f41c1856
giulianoconti <giuliconti1@gmail.com>
2023-11-15 16:46:17 -0300

    miguel feedback

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -7,23 +7,25 @@ export const formatNumber = (value: number, decimals?: number) => {
   }
 
   const valueWithoutDecimals = Math.floor(value);
+  const hasDecimals = value % 1 !== 0;
+  const decimalCount = hasDecimals ? 2 : 0;
 
-  // show formatted value without decimals
+  // show formatted value without decimals for large values
   if (valueWithoutDecimals.toString().length >= 7) {
     return valueWithoutDecimals.toLocaleString();
   }
 
-  // show formatted value with 7 decimals
+  // show formatted value with 7 decimals for small values (zero point something)
   if (valueWithoutDecimals === 0) {
     return value.toLocaleString(undefined, {
-      minimumFractionDigits: 0,
+      minimumFractionDigits: decimalCount,
       maximumFractionDigits: 7,
     });
   }
 
   // show formatted value with 2 decimals
   return value.toLocaleString(undefined, {
-    minimumFractionDigits: 0,
+    minimumFractionDigits: decimalCount,
     maximumFractionDigits: 2,
   });
 };
