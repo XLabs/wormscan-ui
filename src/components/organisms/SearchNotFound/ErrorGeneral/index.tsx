2838a44f821ebe14548febb0ee4187a184a5b1e0
giulianoconti <giuliconti1@gmail.com>
2023-11-21 11:54:43 -0300

    error boundary added

---
+++ src/components/organisms/SearchNotFound/ErrorGeneral/index.tsx
@@ -0,0 +1,47 @@
+import ErrorGeneralImage from "src/assets/errorGeneral.svg";
+import { DISCORD_URL } from "src/consts";
+
+const ErrorGeneral = () => {
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className="error-page error-page-bg-general">
+      <div className="error-page-container">
+        <h2 className="error-page-container-title">
+          <span>Something</span>
+          <span>unexpected</span>
+          <span>happened</span>
+        </h2>
+        <div className="error-page-container-image">
+          <img
+            className="error-page-container-image-general"
+            src={ErrorGeneralImage}
+            alt="Error 500"
+            loading="lazy"
+          />
+        </div>
+        <div className="error-page-container-body">
+          <p className="error-page-container-body-description">Please check again later.</p>
+          <p className="error-page-container-body-description">
+            If the problem persists,{" "}
+            <em className="error-page-container-body-description-mobile">please reach</em>
+            <span>
+              <em className="error-page-container-body-description-desktop">please reach</em> out to
+              us on{" "}
+              <a href={DISCORD_URL} target="_blank" rel="noopener noreferrer">
+                discord.
+              </a>
+            </span>
+          </p>
+          <button className="error-page-container-body-button" onClick={reloadPage}>
+            Reload page
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorGeneral;
