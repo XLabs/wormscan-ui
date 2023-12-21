25cb529b20f12429160413d13935bded78ee86b7
giulianoconti <giuliconti1@gmail.com>
2023-12-21 12:35:43 -0300

    Policy privacy banner added

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Loader } from "src/components/atoms";
+import PolicyPrivacyBanner from "src/components/molecules/PolicyPrivacyBanner";
 import { BaseLayout } from "src/layouts/BaseLayout";
 import { EnvironmentProvider } from "src/context/EnvironmentContext";
 import { ScrollControl } from "src/utils/scrollControl";
@@ -19,6 +20,7 @@ const Navigation = () => {
       <AnalyticsLinkTracker>
         <EnvironmentProvider>
           <ErrorBoundary>
+            <PolicyPrivacyBanner />
             <Suspense
               fallback={
                 <BaseLayout>
