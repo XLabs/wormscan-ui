81c04d19daebc0127a379f873e6803630f4851b1
Valentino Conti <41705567+valentinoConti@users.noreply.github.com>
2024-02-21 10:24:46 -0300

    Redesign Transactions Page (#534)
    * Redesign Transactions Page
    
    * auto review
    
    * text

--- src/components/molecules/StatusBadge/index.tsx
+++ src/components/molecules/StatusBadge/index.tsx
@@ -1,29 +1,69 @@
-import { CheckCircledIcon, ClockIcon, ExclamationTriangleIcon } from "@radix-ui/react-icons";
-import { Chip } from "src/components/atoms";
-import { colorStatus } from "src/consts";
-import { TxStatus } from "src/types";
+import { CheckCircledIcon, CheckIcon, ClockIcon } from "@radix-ui/react-icons";
+import { Chip, Tooltip } from "src/components/atoms";
+import { IStatus } from "src/consts";
 import "./styles.scss";
 
 type Props = {
-  status: TxStatus;
+  STATUS: IStatus;
 };
 
-const StatusBadge = ({ status }: Props) => {
-  const parsedStatus: string = String(status).toUpperCase();
-  const icon =
-    parsedStatus === "COMPLETED" ? (
-      <CheckCircledIcon />
-    ) : parsedStatus === "ONGOING" ? (
-      <ClockIcon />
-    ) : (
-      <ExclamationTriangleIcon />
-    );
-
+const StatusBadge = ({ STATUS }: Props) => {
   return (
-    <Chip color={colorStatus[parsedStatus]} className="status-badge">
-      {icon} {parsedStatus}
-    </Chip>
+    <div className="status-badge">
+      {STATUS === "EXTERNAL_TX" && <StatusExternalTx />}
+      {STATUS === "COMPLETED" && <StatusCompleted />}
+      {STATUS === "IN_PROGRESS" && <StatusInProgress />}
+      {STATUS === "PENDING_REDEEM" && <StatusPendingRedeem />}
+      {STATUS === "VAA_EMITTED" && <StatusVaaEmitted />}
+    </div>
   );
 };
 
 export default StatusBadge;
+
+const StatusInProgress = () => (
+  <Chip className="status-badge-status" color="progress">
+    <ClockIcon height={16} width={16} />
+    IN PROGRESS
+  </Chip>
+);
+
+const StatusVaaEmitted = () => (
+  <Chip className="status-badge-status" color="emitted">
+    <CheckIcon height={16} width={16} />
+    VAA EMITTED
+  </Chip>
+);
+
+const StatusExternalTx = () => (
+  <Chip className="status-badge-status" color="emitted">
+    <CheckIcon height={16} width={16} />
+    EXTERNAL TX
+  </Chip>
+);
+
+const StatusPendingRedeem = () => (
+  <Tooltip
+    side="right"
+    tooltip={
+      <div className="status-badge-tooltip">
+        Your transaction has been completed on the blockchain, but has not yet been redeemed.
+      </div>
+    }
+    type="info"
+  >
+    <div>
+      <Chip className="status-badge-status" color="progress">
+        <ClockIcon height={16} width={16} />
+        PENDING REDEEM
+      </Chip>
+    </div>
+  </Tooltip>
+);
+
+const StatusCompleted = () => (
+  <Chip className="status-badge-status" color="completed">
+    <CheckCircledIcon height={16} width={16} />
+    COMPLETED
+  </Chip>
+);
--- src/types.ts
+++
@@ -1,1 +0,0 @@
-export type TxStatus = "COMPLETED" | "ONGOING" | "FAILED" | "ERROR";
