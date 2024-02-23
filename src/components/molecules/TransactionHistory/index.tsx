8f505643f2d43857c330f71ab9b371cc8938e15f
Giuliano Conti <giuliconti1@gmail.com>
2024-02-22 22:50:59 -0300

    Added protocol stats (#533)
    * protocols stats added
    
    * txHistory text fixed
    
    * tablet
    
    * no decimals
    
    * sorted data
    
    * comment removed
    
    * tooltips
    
    * clg
    
    * fix sorted data
    
    * comment removed
    
    * test

--- src/components/molecules/TransactionHistory/index.tsx
+++ src/components/molecules/TransactionHistory/index.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import PortalStatsImage from "src/assets/portal-stats.svg";
 import { ToggleGroup } from "src/components/atoms";
 import { DateRange } from "src/api/guardian-network/types";
 import { TransactionHistoryChart } from "..";
@@ -22,9 +21,7 @@ const TransactionHistory = () => {
   return (
     <div className="tx-history">
       <div className="tx-history-options">
-        <div className="tx-title">
-          Transaction History <img src={PortalStatsImage} alt="portal logo" width="101" />
-        </div>
+        <div className="tx-title">Transaction History</div>
         <ToggleGroup
           value={selectedRange}
           onValueChange={value => {
--- src/pages/Home/Statistics/index.tsx
+++ src/pages/Home/Statistics/index.tsx
@@ -1,39 +1,14 @@
-import { PortalStats, TransactionHistory, WormholeStats } from "src/components/molecules";
-import { useQuery } from "react-query";
-import { getClient } from "src/api/Client";
+import { ProtocolsStats, TransactionHistory, WormholeStats } from "src/components/molecules";
 import "./styles.scss";
 
 const Statistics = () => {
-  const {
-    isLoading,
-    isError,
-    data: scoreData,
-  } = useQuery("scoresResponse", () => getClient().guardianNetwork.getScores());
-
-  const {
-    "24h_messages": messages24h,
-    "24h_volume": volume24h,
-    total_messages,
-    total_volume,
-  } = scoreData || {};
-
   return (
     <section className="home-statistics">
       <div>
-        <WormholeStats
-          isError={isError}
-          isLoading={isLoading}
-          messages24h={messages24h}
-          total_messages={total_messages}
-        />
+        <WormholeStats />
       </div>
       <div>
-        <PortalStats
-          isError={isError}
-          isLoading={isLoading}
-          total_volume={total_volume}
-          volume24h={volume24h}
-        />
+        <ProtocolsStats />
         <TransactionHistory />
       </div>
     </section>
