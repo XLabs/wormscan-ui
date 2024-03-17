1253d38fe5b5af1c3c318a97e18572ce8b782142
Valentino Conti <valeen.conti@hotmail.com>
2024-03-17 14:15:27 -0300

    hash/id search, better inputs

--- src/pages/VaaParser/index.tsx
+++ src/pages/VaaParser/index.tsx
@@ -26,78 +26,111 @@ const VaaParser = () => {
   const navigate = useNavigateCustom();
 
   const textareaRef = useRef(null);
-  const textareaHexRef = useRef(null);
+  const inputTxRef = useRef(null);
 
-  const [input, setInput] = useState(processInput(vaaParam));
+  const [input, setInput] = useState(processInputValue(vaaParam));
+  const [inputType, setInputType] = useState(processInputType(vaaParam));
+  const [txSearch, setTxSearch] = useState("");
+
   const [result, setResult] = useState<GetParsedVaaOutput>(null);
 
-  const { isError, isLoading, isFetching } = useQuery(
-    ["getParsedVaa", input],
-    () => getClient().guardianNetwork.getParsedVaa(input),
+  const {
+    isError,
+    isLoading: isLoadingParse,
+    isFetching: isFetchingParse,
+  } = useQuery(["getParsedVaa", input], () => getClient().guardianNetwork.getParsedVaa(input), {
+    enabled: !!input,
+    retry: 0,
+    onSuccess: data => {
+      setResult(data);
+      const vaaID = `${data?.vaa?.emitterChain}/${data?.vaa?.emitterAddress}/${data?.vaa?.sequence}`;
+      if (!txSearch) {
+        setTxSearch(vaaID);
+      }
+
+      setTimeout(() => {
+        document.querySelectorAll(".json-view-key").forEach(a => {
+          // Add chain names to decoded VAA
+          if (
+            a.innerHTML?.includes("fromChain") ||
+            a.innerHTML?.includes("toChain") ||
+            a.innerHTML?.includes("tokenChain") ||
+            a.innerHTML?.includes("emitterChain") ||
+            a.innerHTML?.includes("refundChainId") ||
+            a.innerHTML?.includes("recipientChain") ||
+            a.innerHTML?.includes("targetChainId") ||
+            a.innerHTML?.includes("feeChain")
+          ) {
+            const parentElement = a.parentElement;
+
+            const chain = getChainName({
+              chainId: +parentElement.children?.[1]?.innerHTML as ChainId,
+              network: "MAINNET",
+            });
+
+            if (chain) {
+              const newText = document.createElement("span");
+              newText.textContent = ` // ${chain}`;
+
+              a.parentElement?.appendChild(newText);
+            }
+          }
+
+          // Add payload types to decoded VAA
+          if (a.innerHTML?.includes("payloadType")) {
+            const parentElement = a.parentElement;
+
+            const type = txType[+parentElement.children?.[1]?.innerHTML] ?? false;
+
+            if (type) {
+              const newText = document.createElement("span");
+              newText.textContent = ` // ${type}`;
+
+              a.parentElement?.appendChild(newText);
+            }
+          }
+
+          // Add timestamps as texts in decoded VAA
+          if (a.innerHTML?.includes("timestamp")) {
+            const parentElement = a.parentElement;
+
+            const timestamp = parentElement.children?.[1]?.innerHTML?.replaceAll('"', "");
+            const time = new Date(timestamp);
+            const formatted = formatDate(time);
+
+            if (formatted) {
+              const newText = document.createElement("span");
+              newText.textContent = ` // ${formatted}`;
+
+              a.parentElement?.appendChild(newText);
+            }
+          }
+        });
+      }, 150);
+    },
+  });
+
+  const { isLoading: isLoadingTx, isFetching: isFetchingTx } = useQuery(
+    ["getOperations", txSearch],
+    () => {
+      const isVaaID = txSearch.split("/")?.length === 3;
+      const send = isVaaID ? { vaaID: txSearch } : { txHash: txSearch };
+
+      return getClient().guardianNetwork.getOperations(send);
+    },
     {
-      enabled: !!input,
       retry: 0,
+      enabled: !!txSearch && !input,
       onSuccess: data => {
-        setResult(data);
-        setTimeout(() => {
-          document.querySelectorAll(".json-view-key").forEach(a => {
-            // Add chain names to decoded VAA
-            if (
-              a.innerHTML?.includes("fromChain") ||
-              a.innerHTML?.includes("toChain") ||
-              a.innerHTML?.includes("tokenChain") ||
-              a.innerHTML?.includes("emitterChain") ||
-              a.innerHTML?.includes("refundChainId") ||
-              a.innerHTML?.includes("recipientChain") ||
-              a.innerHTML?.includes("targetChainId") ||
-              a.innerHTML?.includes("feeChain")
-            ) {
-              const parentElement = a.parentElement;
+        if (data.length && data[0].vaa?.raw) {
+          const rawVAA = data[0].vaa?.raw;
 
-              const chain = getChainName({
-                chainId: +parentElement.children?.[1]?.innerHTML as ChainId,
-                network: "MAINNET",
-              });
+          setInput(rawVAA);
+          setInputType("base64");
 
-              if (chain) {
-                const newText = document.createElement("span");
-                newText.textContent = ` // ${chain}`;
-
-                a.parentElement?.appendChild(newText);
-              }
-            }
-
-            // Add payload types to decoded VAA
-            if (a.innerHTML?.includes("payloadType")) {
-              const parentElement = a.parentElement;
-
-              const type = txType[+parentElement.children?.[1]?.innerHTML] ?? false;
-
-              if (type) {
-                const newText = document.createElement("span");
-                newText.textContent = ` // ${type}`;
-
-                a.parentElement?.appendChild(newText);
-              }
-            }
-
-            // Add timestamps as texts in decoded VAA
-            if (a.innerHTML?.includes("timestamp")) {
-              const parentElement = a.parentElement;
-
-              const timestamp = parentElement.children?.[1]?.innerHTML?.replaceAll('"', "");
-              const time = new Date(timestamp);
-              const formatted = formatDate(time);
-
-              if (formatted) {
-                const newText = document.createElement("span");
-                newText.textContent = ` // ${formatted}`;
-
-                a.parentElement?.appendChild(newText);
-              }
-            }
-          });
-        }, 150);
+          navigate(`/vaa-parser/${rawVAA}`, { replace: true });
+          inputTxRef.current?.blur();
+        }
       },
     },
   );
@@ -107,6 +140,8 @@ const VaaParser = () => {
       ? `${result?.vaa?.emitterChain}/${result?.vaa?.emitterAddress}/${result?.vaa?.sequence}`
       : "";
 
+  const isLoading = isLoadingParse || isFetchingParse || isLoadingTx || isFetchingTx;
+
   return (
     <BaseLayout>
       <div className="devtools-page">
@@ -114,45 +149,71 @@ const VaaParser = () => {
           <h1 className="devtools-page-title">VAA Parser</h1>
           <div className="devtools-page-body">
             <div className="parse">
+              <div className="parse-txType">
+                <label htmlFor="parse-txType-input">TxHash/VaaID search</label>
+                <input
+                  type="text"
+                  className="parse-txType-input"
+                  id="parse-txType-input"
+                  ref={inputTxRef}
+                  value={txSearch}
+                  onChange={e => {
+                    setInput("");
+                    console.log(e.target.value);
+
+                    setTxSearch(e.target.value);
+                    inputTxRef?.current?.blur();
+                  }}
+                  name="txType-input"
+                  aria-label="Transaction hash or VAA ID input"
+                  spellCheck={false}
+                />
+              </div>
               <div className="parse-input-container">
-                <label htmlFor="parse-input">Base64 Encoded VAA</label>
+                <label htmlFor="parse-input">Encoded VAA</label>
                 <textarea
                   className="parse-input"
                   id="parse-input"
                   disabled={false}
-                  value={input}
+                  value={inputType === "base64" ? input : base64ToHex(input)}
                   ref={textareaRef}
                   onChange={e => {
-                    const newInput = processInput(e.target.value);
+                    const targetValue = e.target.value;
+
+                    const newInput = processInputValue(targetValue);
                     setInput(newInput);
+                    setInputType(processInputType(targetValue));
+                    setTxSearch("");
+
                     navigate(`/vaa-parser/${newInput}`, { replace: true });
                     textareaRef?.current?.blur();
                   }}
                   name="VAA-Input"
-                  placeholder="AQA..."
+                  placeholder={`base64/hex vaa..`}
                   aria-label="Base64 VAA input"
                   draggable={false}
                   spellCheck={false}
                 />
-                <label htmlFor="parse-input-hex">Hex Encoded VAA</label>
-                <textarea
-                  className="parse-input"
-                  id="parse-input-hex"
-                  disabled={false}
-                  value={input ? "0x" + base64ToHex(input) : input}
-                  ref={textareaHexRef}
-                  onChange={e => {
-                    const newInput = processInput(e.target.value);
-                    setInput(newInput);
-                    navigate(`/vaa-parser/${newInput}`, { replace: true });
-                    textareaHexRef?.current?.blur();
-                  }}
-                  name="VAA-Input-Hex"
-                  placeholder="0x..."
-                  aria-label="Hex VAA input"
-                  draggable={false}
-                  spellCheck={false}
-                />
+                {input && (
+                  <div className="parse-input-container-format">
+                    <span
+                      onClick={() => {
+                        setInputType("hex");
+                      }}
+                      className={inputType === "hex" ? "active" : ""}
+                    >
+                      Hex
+                    </span>
+                    <span
+                      onClick={() => {
+                        setInputType("base64");
+                      }}
+                      className={inputType === "base64" ? "active" : ""}
+                    >
+                      Base64
+                    </span>
+                  </div>
+                )}
               </div>
               <div className="parse-result" id="parse-result" aria-label="Parsed result">
                 <div className="parse-result-title">Decoded VAA</div>
@@ -166,12 +227,12 @@ const VaaParser = () => {
 
                 {isError ? (
                   <span className="parse-result-not-found">Parsing failed</span>
-                ) : isLoading || isFetching ? (
+                ) : isLoading ? (
                   <Loader />
                 ) : (
                   <div className="parse-result-json">
                     {!!result && input && VAA_ID && (
-                      <div>
+                      <div className="parse-result-json-text">
                         <JsonText data={result} />
                         <NavLink target="_blank" to={`/tx/${VAA_ID}`}>
                           <div className="parse-result-bottom">
@@ -194,14 +255,23 @@ const VaaParser = () => {
 
 export default VaaParser;
 
-const processInput = (str: string) => {
+const processInputValue = (str: string) => {
   const input = str?.startsWith("0x") ? str.replace("0x", "") : str;
   const hexRegExp = /^[0-9a-fA-F]+$/;
+  const isHex = hexRegExp.test(input);
 
-  // isHex (ensuring even length)
-  if (hexRegExp.test(input) /* && input.length % 2 === 0 */) {
+  if (isHex) {
     return hexToBase64(input);
   }
 
   return input || "";
+};
+
+const processInputType = (str: string): "base64" | "hex" => {
+  const input = str?.startsWith("0x") ? str.replace("0x", "") : str;
+  const hexRegExp = /^[0-9a-fA-F]+$/;
+  const isHex = hexRegExp.test(input);
+
+  if (isHex) return "hex";
+  return "base64";
 };
