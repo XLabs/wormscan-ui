c03356dad9d5d47ab52b19cf2f06ad9be8783019
Valentino Conti <valeen.conti@hotmail.com>
2024-04-12 10:14:50 -0300

    Attestation txns advanced view improvement

--- src/pages/Tx/Information/AdvancedView/Details/index.tsx
+++ src/pages/Tx/Information/AdvancedView/Details/index.tsx
@@ -17,6 +17,7 @@ const Details = ({
   fromChain,
   fromChainOrig,
   guardianSignaturesCount,
+  isAttestation,
   isGatewaySource,
   isUnknownApp,
   originDateParsed,
@@ -153,11 +154,11 @@ const Details = ({
           </div>
         </div>
         <div className="tx-details-group-line">
-          <div className="tx-details-group-line-key">Amount</div>
+          <div className="tx-details-group-line-key">{isAttestation ? "Token" : "Amount"}</div>
           <div className="tx-details-group-line-value">
             {tokenAmount ? (
               <>
-                {amountSent}{" "}
+                {!isAttestation ? amountSent : ""}{" "}
                 {sourceSymbol ? (
                   <>
                     {sourceTokenLink ? (
@@ -243,118 +244,123 @@ const Details = ({
           </div>
         </div>
       </div>
-      <div className="tx-details-group">
-        <div className="tx-details-group-line">
-          <div className="tx-details-group-line-key">Target Chain</div>
-          <div className="tx-details-group-line-value">
-            {toChain ? (
-              <>
-                <BlockchainIcon chainId={toChain} network={currentNetwork} size={24} />
-                {getChainName({ chainId: toChain, network: currentNetwork }).toUpperCase()}
-                {parsedPayload?.["gateway_transfer"] && (
-                  <span className="comment"> (through Gateway)</span>
-                )}
-              </>
-            ) : (
-              "N/A"
-            )}
+
+      {(!isAttestation || (isAttestation && toChain)) && (
+        <div className="tx-details-group">
+          <div className="tx-details-group-line">
+            <div className="tx-details-group-line-key">Target Chain</div>
+            <div className="tx-details-group-line-value">
+              {toChain ? (
+                <>
+                  <BlockchainIcon chainId={toChain} network={currentNetwork} size={24} />
+                  {getChainName({ chainId: toChain, network: currentNetwork }).toUpperCase()}
+                  {parsedPayload?.["gateway_transfer"] && (
+                    <span className="comment"> (through Gateway)</span>
+                  )}
+                </>
+              ) : (
+                "N/A"
+              )}
+            </div>
           </div>
-        </div>
-        <div className="tx-details-group-line">
-          <div className="tx-details-group-line-key">Destination Address</div>
-          <div className="tx-details-group-line-value">
-            {parsedDestinationAddress ? (
-              <>
-                <a
-                  href={getExplorerLink({
-                    network: currentNetwork,
-                    chainId: toChain,
-                    value: parsedDestinationAddress,
-                    base: "address",
-                    isNativeAddress: true,
-                  })}
-                  target="_blank"
-                  rel="noopener noreferrer"
+          <div className="tx-details-group-line">
+            <div className="tx-details-group-line-key">Destination Address</div>
+            <div className="tx-details-group-line-value">
+              {parsedDestinationAddress ? (
+                <>
+                  <a
+                    href={getExplorerLink({
+                      network: currentNetwork,
+                      chainId: toChain,
+                      value: parsedDestinationAddress,
+                      base: "address",
+                      isNativeAddress: true,
+                    })}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <TruncateText
+                      containerWidth={lineValueWidth}
+                      extraWidth={extraWidthUnknownApp}
+                      text={parsedDestinationAddress.toUpperCase()}
+                    />
+                  </a>{" "}
+                  <CopyToClipboard toCopy={parsedDestinationAddress}>
+                    <CopyIcon height={20} width={20} />
+                  </CopyToClipboard>
+                </>
+              ) : (
+                "N/A"
+              )}
+              {isUnknownApp && (
+                <Tooltip
+                  tooltip={
+                    <div>
+                      Address shown corresponds to a Smart Contract handling the transaction. Funds
+                      will be sent to your recipient address.
+                    </div>
+                  }
+                  type="info"
                 >
-                  <TruncateText
-                    containerWidth={lineValueWidth}
-                    extraWidth={extraWidthUnknownApp}
-                    text={parsedDestinationAddress.toUpperCase()}
-                  />
-                </a>{" "}
-                <CopyToClipboard toCopy={parsedDestinationAddress}>
-                  <CopyIcon height={20} width={20} />
-                </CopyToClipboard>
-              </>
-            ) : (
-              "N/A"
-            )}
-            {isUnknownApp && (
-              <Tooltip
-                tooltip={
-                  <div>
-                    Address shown corresponds to a Smart Contract handling the transaction. Funds
-                    will be sent to your recipient address.
-                  </div>
-                }
-                type="info"
-              >
-                <InfoCircledIcon />
-              </Tooltip>
-            )}
+                  <InfoCircledIcon />
+                </Tooltip>
+              )}
+            </div>
           </div>
-        </div>
-        <div className="tx-details-group-line">
-          <div className="tx-details-group-line-key">Redeem Amount</div>
-          <div className="tx-details-group-line-value">
-            {Number(fee) ? (
-              <>
-                {redeemedAmount}{" "}
-                {targetSymbol &&
-                  (targetTokenLink ? (
-                    <a href={targetTokenLink} target="_blank" rel="noopener noreferrer">
-                      {targetSymbol}
-                    </a>
-                  ) : (
-                    <span>{targetSymbol}</span>
-                  ))}
-              </>
-            ) : tokenAmount ? (
-              <>
-                {amountSent}{" "}
-                {targetSymbol ? (
-                  <>
-                    {targetTokenLink ? (
+          <div className="tx-details-group-line">
+            <div className="tx-details-group-line-key">
+              {isAttestation ? "Target Token" : "Redeem Amount"}
+            </div>
+            <div className="tx-details-group-line-value">
+              {Number(fee) ? (
+                <>
+                  {!isAttestation ? redeemedAmount : ""}{" "}
+                  {targetSymbol &&
+                    (targetTokenLink ? (
                       <a href={targetTokenLink} target="_blank" rel="noopener noreferrer">
                         {targetSymbol}
                       </a>
                     ) : (
                       <span>{targetSymbol}</span>
-                    )}
-                    {amountSentUSD && `(${amountSentUSD} USD)`}
-                  </>
-                ) : (
-                  "N/A"
-                )}
-              </>
-            ) : (
-              "N/A"
-            )}
-          </div>
-        </div>
-        {showMetaMaskBtn && (
-          <div className="tx-details-group-line">
-            <div className="tx-details-group-line-key"></div>
-            <div className="tx-details-group-line-value">
-              <AddToMetaMaskBtn
-                currentNetwork={currentNetwork}
-                toChain={toChain as ChainId}
-                tokenInfo={tokenInfo}
-              />
+                    ))}
+                </>
+              ) : tokenAmount ? (
+                <>
+                  {!isAttestation ? amountSent : ""}{" "}
+                  {targetSymbol ? (
+                    <>
+                      {targetTokenLink ? (
+                        <a href={targetTokenLink} target="_blank" rel="noopener noreferrer">
+                          {targetSymbol}
+                        </a>
+                      ) : (
+                        <span>{targetSymbol}</span>
+                      )}
+                      {amountSentUSD && `(${amountSentUSD} USD)`}
+                    </>
+                  ) : (
+                    "N/A"
+                  )}
+                </>
+              ) : (
+                "N/A"
+              )}
             </div>
           </div>
-        )}
-      </div>
+          {showMetaMaskBtn && (
+            <div className="tx-details-group-line">
+              <div className="tx-details-group-line-key"></div>
+              <div className="tx-details-group-line-value">
+                <AddToMetaMaskBtn
+                  currentNetwork={currentNetwork}
+                  toChain={toChain as ChainId}
+                  tokenInfo={tokenInfo}
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
