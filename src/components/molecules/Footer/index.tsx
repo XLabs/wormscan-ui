1ef9ac7c986bfadb8fc8284c7036fda6b90693f1
Giuliano Conti <giuliconti1@gmail.com>
2024-04-16 10:19:34 -0300

    New Footer (#627)
    * new footer added
    
    * NavLink
    
    * max-width
    
    * discord radix icon removed
    
    * hover
    
    * Valens feedback

--- src/components/molecules/Footer/index.tsx
+++ src/components/molecules/Footer/index.tsx
@@ -1,17 +1,21 @@
 import { useTranslation } from "react-i18next";
-import { PinTopIcon } from "@radix-ui/react-icons";
+import { GitHubLogoIcon, PinTopIcon } from "@radix-ui/react-icons";
 import { NavLink, Tag } from "src/components/atoms";
+import { JoinUs, WormholeBrand } from "src/components/molecules";
 import {
   DISCORD_URL,
+  GITHUB_URL,
+  PROVIDE_FEEDBACK_URL,
   TWITTER_URL,
-  WORMHOLE_DOCS_URL,
+  WORMHOLE_BLOG,
+  XLABS_ABOUT_US_URL,
   XLABS_CAREERS_URL,
+  XLABS_OUR_WORK_URL,
   XLABS_URL,
 } from "src/consts";
 import DiscordIcon from "src/icons/DiscordIcon";
 import TwitterIcon from "src/icons/TwitterIcon";
 import XlabsIcon from "src/icons/XlabsIcon";
-import { WormholeBrand } from "src/components/molecules";
 import packageJson from "../../../../package.json";
 import "./styles.scss";
 
@@ -26,97 +30,105 @@ const Footer = () => {
   return (
     <footer className="footer" data-testid="footer">
       <div className="footer-container">
-        <div className="footer-top">
-          <div className="footer-top-brand">
-            <NavLink to="/" data-testid="footer-logo-link">
-              <WormholeBrand size="regular" />
-            </NavLink>
-          </div>
+        <NavLink to="/" data-testid="footer-logo-link">
+          <WormholeBrand size="regular" />
+        </NavLink>
 
-          <div className="footer-top-button-container">
-            <button type="button" className="footer-top-button" onClick={goTop}>
-              <PinTopIcon /> Back to Top
-            </button>
-          </div>
-        </div>
+        <button className="footer-container-button" type="button" onClick={goTop}>
+          <PinTopIcon /> {t("home.footer.backToTop")}
+        </button>
+      </div>
 
-        <div className="footer-social">
-          <div className="footer-social-container">
-            <div className="footer-social-text">{t("home.footer.joinUs")}</div>
-            <div className="footer-social-icons">
-              <a
-                href={DISCORD_URL}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Discord link"
-              >
-                <DiscordIcon />
-              </a>
-              <a
-                href={TWITTER_URL}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Twitter link"
-              >
-                <TwitterIcon />
-              </a>
-            </div>
-          </div>
-        </div>
+      <div className="footer-container">
+        <div className="footer-container-links">
+          <div className="footer-container-links-container">
+            <p className="footer-container-links-container-title">{t("home.footer.community")}</p>
 
-        <div className="footer-links" data-testid="footer-nav">
-          <div className="footer-links-item" data-link="home">
-            <NavLink to="/">{t("home.footer.home")}</NavLink>
-          </div>
-
-          <div className="footer-links-item" data-link="txs">
-            <NavLink to="/txs">{t("home.footer.txs")}</NavLink>
-          </div>
-
-          <div className="footer-links-item" data-link="contact">
-            <div className="footer-links-item">
-              <a href={DISCORD_URL} target="_blank" rel="noopener noreferrer">
-                {t("home.footer.contactUs")}
-              </a>
-            </div>
-          </div>
-
-          <div className="footer-links-item">
-            <a href={XLABS_CAREERS_URL} target="_blank" rel="noopener noreferrer">
-              {t("home.footer.careers")}
+            <a
+              href={WORMHOLE_BLOG}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Wormhole blog link"
+            >
+              {t("home.footer.blog")}
             </a>
-            <Tag type="chip" size="small">
-              {t("home.footer.hiring")}
-            </Tag>
-          </div>
-
-          <div className="footer-links-item" data-link="api">
-            <a href={WORMHOLE_DOCS_URL} target="_blank" rel="noopener noreferrer">
-              {t("home.footer.apiDoc")}
+            <a
+              href={TWITTER_URL}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Twitter link"
+            >
+              <TwitterIcon />
+            </a>
+            <a
+              href={DISCORD_URL}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Discord link"
+            >
+              <DiscordIcon />
+            </a>
+            <a href={GITHUB_URL} target="_blank" rel="noopener noreferrer" aria-label="GitHub link">
+              <GitHubLogoIcon />
             </a>
           </div>
 
-          <div className="footer-links-item" data-link="termsOfUse">
+          <div className="footer-container-links-container">
+            <p className="footer-container-links-container-title">{t("home.footer.company")}</p>
+
+            <a
+              href={XLABS_ABOUT_US_URL}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="About Us link"
+            >
+              {t("home.footer.aboutUs")}
+            </a>
+            <a
+              href={XLABS_OUR_WORK_URL}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Our Work link"
+            >
+              {t("home.footer.ourWork")}
+            </a>
+            <div>
+              <a
+                href={XLABS_CAREERS_URL}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Careers link"
+              >
+                {t("home.footer.careers")}
+              </a>
+              <Tag type="chip" size="small">
+                {t("home.footer.hiring")}
+              </Tag>
+            </div>
+            <a href={PROVIDE_FEEDBACK_URL} target="_blank" rel="noopener noreferrer">
+              {t("home.footer.provideFeedback")}
+            </a>
             <NavLink to="/terms-of-use">{t("home.footer.termsOfUse")}</NavLink>
           </div>
         </div>
 
-        <div className="footer-built">
-          <div className="footer-built-container">
-            <div className="footer-built-text">Built by</div>
-            <div className="footer-built-icons">
-              <a href={XLABS_URL} target="_blank" rel="noopener noreferrer" aria-label="xLabs link">
-                <XlabsIcon width={36} height={44} />
-              </a>
-            </div>
-          </div>
-        </div>
+        <JoinUs />
 
-        <div className="footer-copy">
-          <div className="footer-copy-brand">
-            &copy; {year} Wormholescan v{packageJson.version}
+        <div className="footer-container-build">
+          <div className="footer-container-build-container">
+            <p className="footer-container-build-container-text">{t("home.footer.builtBy")}</p>
+
+            <a href={XLABS_URL} target="_blank" rel="noopener noreferrer" aria-label="xLabs link">
+              <XlabsIcon width={36} height={44} />
+            </a>
           </div>
         </div>
+      </div>
+
+      <div className="footer-container">
+        <p className="footer-container-copy">
+          &copy; {year} Wormholescan v{packageJson.version}
+        </p>
       </div>
     </footer>
   );
