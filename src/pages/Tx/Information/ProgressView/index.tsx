d926fe3207de86f4202ed2e4a8404086572448e7
giulianoconti <giuliconti1@gmail.com>
2024-09-25 16:21:41 -0300

    last changes tx details

--- src/pages/Tx/Information/ProgressView/index.tsx
+++ src/pages/Tx/Information/ProgressView/index.tsx
@@ -23,59 +23,74 @@ const ProgressView = ({
   const SOURCE_SYMBOL = sourceTokenInfo?.tokenSymbol || sourceSymbol;
 
   return (
-    <div className="tx-overview-section-row progress-section">
-      <div className="tx-overview-section-row-info">
-        <div className="progress-container">
-          <div className={`progress-title active`}>TRANSACTION PROGRESS</div>
+    <div className="progress-section">
+      <div className="progress-container">
+        <div className={`progress-title active`}>TRANSACTION PROGRESS</div>
 
-          <>
+        <>
+          <div className="progress-item">
+            <div className="progress-icon">
+              <CheckCircle2 />
+            </div>
+
+            <div className="progress-text">
+              <p>
+                {!isJustGenericRelayer && !isMayanOnly && !nftInfo && (
+                  <>
+                    Transfer{" "}
+                    {tokenAmount && (
+                      <>
+                        {!isAttestation ? amountSent : ""}{" "}
+                        {SOURCE_SYMBOL && (
+                          <>
+                            {SOURCE_SYMBOL} {amountSentUSD && `($${amountSentUSD})`}
+                          </>
+                        )}
+                      </>
+                    )}
+                  </>
+                )}{" "}
+                {!isJustGenericRelayer && !isMayanOnly && !nftInfo ? "from" : "From"}{" "}
+                {getChainName({ chainId: fromChain, network: currentNetwork })}{" "}
+                {!!toChain && ` to ${getChainName({ chainId: toChain, network: currentNetwork })}`}
+              </p>
+
+              <span>{originDateParsed}</span>
+            </div>
+          </div>
+
+          {!isJustGenericRelayer && STATUS === "IN_GOVERNORS" && (
             <div className="progress-item">
               <div className="progress-icon">
                 <CheckCircle2 />
               </div>
 
               <div className="progress-text">
-                <p>
-                  {!isJustGenericRelayer && !isMayanOnly && !nftInfo && (
-                    <>
-                      Transfer{" "}
-                      {tokenAmount && (
-                        <>
-                          {!isAttestation ? amountSent : ""}{" "}
-                          {SOURCE_SYMBOL && (
-                            <>
-                              {SOURCE_SYMBOL} {amountSentUSD && `($${amountSentUSD})`}
-                            </>
-                          )}
-                        </>
-                      )}
-                    </>
-                  )}{" "}
-                  {!isJustGenericRelayer && !isMayanOnly && !nftInfo ? "from" : "From"}{" "}
-                  {getChainName({ chainId: fromChain, network: currentNetwork })}{" "}
-                  {!!toChain &&
-                    ` to ${getChainName({ chainId: toChain, network: currentNetwork })}`}
-                </p>
-
-                <span>{originDateParsed}</span>
+                <p>In governor - This transaction will take 24 hours to process</p>
               </div>
             </div>
+          )}
 
-            {!isJustGenericRelayer && STATUS === "IN_GOVERNORS" && (
-              <div className="progress-item">
-                <div className="progress-icon">
-                  <CheckCircle2 />
-                </div>
+          <div
+            className={`progress-item ${
+              STATUS === "IN_PROGRESS" || STATUS === "IN_GOVERNORS" ? "disabled" : ""
+            }`}
+          >
+            <div className="progress-icon">
+              <CheckCircle2 />
+            </div>
 
-                <div className="progress-text">
-                  <p>In governor - This transaction will take 24 hours to process</p>
-                </div>
-              </div>
-            )}
+            <div className="progress-text">
+              <p>VAA signed by wormhole guardians</p>
+            </div>
+          </div>
 
+          {!isJustGenericRelayer && (
             <div
               className={`progress-item ${
-                STATUS === "IN_PROGRESS" || STATUS === "IN_GOVERNORS" ? "disabled" : ""
+                STATUS === "IN_PROGRESS" || STATUS === "IN_GOVERNORS" || STATUS === "VAA_EMITTED"
+                  ? "disabled"
+                  : ""
               }`}
             >
               <div className="progress-icon">
@@ -83,44 +98,26 @@ const ProgressView = ({
               </div>
 
               <div className="progress-text">
-                <p>VAA signed by wormhole guardians</p>
+                <p>
+                  Pending redemption
+                  {!!toChain &&
+                    ` in ${getChainName({ chainId: toChain, network: currentNetwork })}`}
+                </p>
               </div>
             </div>
+          )}
 
-            {!isJustGenericRelayer && (
-              <div
-                className={`progress-item ${
-                  STATUS === "IN_PROGRESS" || STATUS === "IN_GOVERNORS" || STATUS === "VAA_EMITTED"
-                    ? "disabled"
-                    : ""
-                }`}
-              >
-                <div className="progress-icon">
-                  <CheckCircle2 />
-                </div>
-
-                <div className="progress-text">
-                  <p>
-                    Pending redemption
-                    {!!toChain &&
-                      ` in ${getChainName({ chainId: toChain, network: currentNetwork })}`}
-                  </p>
-                </div>
-              </div>
-            )}
-
-            <div className={`progress-item ${STATUS === "COMPLETED" ? "" : "disabled"}`}>
-              <div className="progress-icon">
-                <CheckCircle2 />
-              </div>
-
-              <div className="progress-text">
-                <p>Transactions completed</p>
-                <span>{destinationDateParsed}</span>
-              </div>
+          <div className={`progress-item ${STATUS === "COMPLETED" ? "" : "disabled"}`}>
+            <div className="progress-icon">
+              <CheckCircle2 />
             </div>
-          </>
-        </div>
+
+            <div className="progress-text">
+              <p>Transactions completed</p>
+              <span>{destinationDateParsed}</span>
+            </div>
+          </div>
+        </>
       </div>
     </div>
   );
