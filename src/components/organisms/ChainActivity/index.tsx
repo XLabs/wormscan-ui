9e4139056d2bdb86994dffc6373a5243908bae72
Giuliano Conti <giuliconti1@gmail.com>
2024-10-02 15:45:01 -0300

    Chains chart - little fixes (#850)
    * chains chart little fixes
    
    * x labels fixed
    
    * sankey bottom text fixed

--- src/components/organisms/ChainActivity/index.tsx
+++ src/components/organisms/ChainActivity/index.tsx
@@ -179,7 +179,7 @@ const ChainActivity = () => {
   const tickAmount =
     allChainsSerie?.[0]?.data?.length > 0
       ? isBigDesktop
-        ? Math.min(allChainsSerie[0].data.length, 19)
+        ? Math.min(allChainsSerie[0].data.length, 18)
         : isTablet
         ? Math.min(allChainsSerie[0].data.length, 10)
         : 4
@@ -394,7 +394,7 @@ const ChainActivity = () => {
     setEndDate(new Date());
     setStartDateDisplayed(initialDataDate);
     setEndDateDisplayed(new Date());
-    setLastBtnSelected("all");
+    setLastBtnSelected("year");
     setShowAllSourceChains(true);
     setShowAllTargetChains(true);
     setSourceChainListSelected([ALL_SOURCE_CHAINS]);
@@ -1035,14 +1035,14 @@ const ChainActivity = () => {
                       <div class="chain-activity-chart-tooltip-total-msg">
                         ${
                           showAllSourceChains
-                            ? "<div class='chain-activity-chart-tooltip-container-each-msg-icon' style='background-color: var(--color-plum)'></div>"
+                            ? "<div class='chain-activity-chart-tooltip-container-each-msg-icon' style='background-color: var(--color-lime)'></div>"
                             : ""
                         }
                     
                         ${
                           metricSelected === "transactions"
                             ? `Transfers Sum: <span>${formatNumber(totalY, 0)}</span>`
-                            : `Volume Sum: <span>$${numberToSuffix(totalY)}</span>`
+                            : `Volume Sum: <span>$${formatNumber(totalY, 0)}</span>`
                         }
                       </div>
                       <p class="chain-activity-chart-tooltip-chains">Chains:</p>
