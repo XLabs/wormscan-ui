3975cbd7fa0a453a15ca1d77086a665d8b4b4a33
Valentino Conti <valeen.conti@hotmail.com>
2024-10-07 16:54:58 -0300

    pr fix

--- src/icons/generic/LinearIcon/index.tsx
+++ src/icons/generic/LinearIcon/index.tsx
@@ -8,14 +8,6 @@ const LinearIcon = ({ style, width = 24 }: { style?: React.CSSProperties; width?
     xmlns="http://www.w3.org/2000/svg"
   >
     <path d="M3 21L21 3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-    {/* <path
-      d="M3 21H21V3"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      fill="none"
-    /> */}
   </svg>
 );
 
--- src/icons/generic/LogarithmicIcon/index.tsx
+++ src/icons/generic/LogarithmicIcon/index.tsx
@@ -14,14 +14,6 @@ const LogarithmicIcon = ({
     xmlns="http://www.w3.org/2000/svg"
   >
     <path d="M3 21C3 21 5 3 21 3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-    {/* <path
-      d="M3 21H21V3"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      fill="none"
-    /> */}
   </svg>
 );
 
