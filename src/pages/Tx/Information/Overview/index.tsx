6df9559bfc2d65da177daf93a14205725a727bee
Valentino Conti <valeen.conti@hotmail.com>
2024-10-17 23:13:53 -0300

    only for in governors

--- src/pages/Tx/Information/Overview/index.tsx
+++ src/pages/Tx/Information/Overview/index.tsx
@@ -466,27 +466,31 @@ const Overview = ({
           </div>
         </div>
 
-        <div className="tx-overview-section-row">
-          <h4 className="tx-overview-section-row-title">
-            <Tooltip
-              type="info"
-              tooltip={
-                <div>
-                  <p>The date and time when the transaction will get released by the Governors.</p>
-                </div>
-              }
-            >
-              <span>
-                <InfoCircleIcon /> Release Time
-              </span>
-            </Tooltip>
-          </h4>
-          <div className="tx-overview-section-row-info">
-            <div className="tx-overview-section-row-info-container">
-              <div className="text">{releaseTimestamp ? `${remainingTime}` : "N/A"}</div>
+        {releaseTimestamp && (
+          <div className="tx-overview-section-row">
+            <h4 className="tx-overview-section-row-title">
+              <Tooltip
+                type="info"
+                tooltip={
+                  <div>
+                    <p>
+                      The date and time when the transaction will get released by the Governors.
+                    </p>
+                  </div>
+                }
+              >
+                <span>
+                  <InfoCircleIcon /> Release Time
+                </span>
+              </Tooltip>
+            </h4>
+            <div className="tx-overview-section-row-info">
+              <div className="tx-overview-section-row-info-container">
+                <div className="text">{releaseTimestamp ? `${remainingTime}` : "N/A"}</div>
+              </div>
             </div>
           </div>
-        </div>
+        )}
 
         {destinationDateParsed && (
           <div className="tx-overview-section-row">
