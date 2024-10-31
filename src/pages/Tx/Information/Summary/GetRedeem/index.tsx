3cb8fa1765a12f75ed7699a05060f9c30b866c4d
giulianoconti <giuliconti1@gmail.com>
2024-10-31 13:16:20 -0300

    tooltip removed

--- src/pages/Tx/Information/Summary/GetRedeem/index.tsx
+++ src/pages/Tx/Information/Summary/GetRedeem/index.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { CheckIcon, CrossIcon, Cube3DIcon } from "src/icons/generic";
-import { Tooltip } from "src/components/atoms";
 import "./styles.scss";
 
 interface Props {
@@ -30,8 +29,6 @@ export const GetRedeem = ({ canTryToGetRedeem, foundRedeem, loadingRedeem, getRe
     }, 5000);
   }, [foundRedeem]);
 
-  const showTooltip = !loadingRedeem && !showCheck && !showCross;
-
   useEffect(() => {
     if (showCross) {
       toast("Executed Transaction Not Found", {
@@ -46,57 +43,48 @@ export const GetRedeem = ({ canTryToGetRedeem, foundRedeem, loadingRedeem, getRe
     }
   }, [showCross]);
 
+  if (!shouldShow) return null;
+
   return (
-    <>
-      {shouldShow && (
-        <Tooltip
-          controlled={!showTooltip}
-          open={showTooltip}
-          tooltip={<div>Try to find execution transaction</div>}
-          type="info"
+    <div
+      className={`getRedeem ${showCross || showCheck ? "dissapearing" : ""} ${
+        showCross ? "cross" : ""
+      }`}
+      onClick={() => !loadingRedeem && !(showCross || showCheck) && getRedeem()}
+      style={{
+        cursor: showCross || showCheck ? "default" : "pointer",
+      }}
+    >
+      <div className={`getRedeem-button ${loadingRedeem ? "loading" : ""}`}>
+        <div
+          className="getRedeem-button-icon"
+          style={{
+            opacity: showCheck ? 100 : 0,
+          }}
         >
-          <div
-            className={`getRedeem ${showCross || showCheck ? "dissapearing" : ""} ${
-              showCross ? "cross" : ""
-            } ${showTooltip ? "tooltip" : ""}`}
-            onClick={() => !loadingRedeem && !(showCross || showCheck) && getRedeem()}
-            style={{
-              cursor: !showTooltip ? "default" : "pointer",
-            }}
-          >
-            <div className={`getRedeem-button ${loadingRedeem ? "loading" : ""}`}>
-              <div
-                className="getRedeem-button-icon"
-                style={{
-                  opacity: showCheck ? 100 : 0,
-                }}
-              >
-                <CheckIcon />
-              </div>
+          <CheckIcon />
+        </div>
 
-              <div
-                className="getRedeem-button-icon"
-                style={{
-                  opacity: showCross ? 100 : 0,
-                }}
-              >
-                <CrossIcon />
-              </div>
+        <div
+          className="getRedeem-button-icon"
+          style={{
+            opacity: showCross ? 100 : 0,
+          }}
+        >
+          <CrossIcon />
+        </div>
 
-              <div
-                className="getRedeem-button-icon"
-                style={{
-                  opacity: !showCross && !showCheck ? 100 : 0,
-                  transition: "none",
-                }}
-              >
-                <Cube3DIcon width={24} />
-              </div>
-            </div>
-            <div className="getRedeem-find-txt">Find Execution Tx</div>
-          </div>
-        </Tooltip>
-      )}
-    </>
+        <div
+          className="getRedeem-button-icon"
+          style={{
+            opacity: !showCross && !showCheck ? 100 : 0,
+            transition: "none",
+          }}
+        >
+          <Cube3DIcon width={24} />
+        </div>
+      </div>
+      <div className="getRedeem-find-txt">Search Execution Tx</div>
+    </div>
   );
 };
