5f2ab045f3be1f8c23024140bc344dc7dd340f41
giulianoconti <giuliconti1@gmail.com>
2024-12-05 15:42:48 -0300

    show msg

--- src/pages/Tx/Information/ProgressView/index.tsx
+++ src/pages/Tx/Information/ProgressView/index.tsx
@@ -1,17 +1,14 @@
-import { NavLink } from "src/components/atoms";
 import { CheckCircle2, ClockIcon } from "src/icons/generic";
 import { getChainName } from "src/utils/wormhole";
 import { OverviewProps } from "src/utils/txPageUtils";
-import "./styles.scss";
-import { parseTx } from "src/utils/crypto";
 import { getRemainingTime } from "src/utils/date";
+import "./styles.scss";
 
 const ProgressView = ({
   amountSent,
   amountSentUSD,
   currentNetwork,
   destinationDateParsed,
-  emitterChainId,
   fromChain,
   isAttestation,
   isJustGenericRelayer,
@@ -26,14 +23,8 @@ const ProgressView = ({
   targetTxHash,
   toChain,
   tokenAmount,
-  txHash,
 }: OverviewProps) => {
   const SOURCE_SYMBOL = sourceTokenInfo?.tokenSymbol || sourceSymbol;
-
-  const parseTxHash = parseTx({
-    value: txHash,
-    chainId: emitterChainId,
-  });
 
   const remainingTime = getRemainingTime(releaseTimestamp);
 
@@ -58,31 +49,26 @@ const ProgressView = ({
           </div>
         </div>
 
-        {(payloadType === 1 || payloadType === 3) &&
-          !isJustGenericRelayer &&
-          !isMayanOnly &&
-          !nftInfo &&
-          tokenAmount && (
-            <div className="progress-item">
-              <div className="progress-icon">
-                <CheckCircle2 />
-              </div>
+        {payloadType !== 2 && !isJustGenericRelayer && !isMayanOnly && !nftInfo && tokenAmount && (
+          <div className="progress-item">
+            <div className="progress-icon">
+              <CheckCircle2 />
+            </div>
 
-              <div className="progress-text">
-                <div className="progress-text-p">
-                  Transfer {!isAttestation ? amountSent : ""}{" "}
-                  {SOURCE_SYMBOL && (
-                    <>
-                      {SOURCE_SYMBOL} {amountSentUSD && `($${amountSentUSD}) `}
-                    </>
-                  )}
-                  {getChainName({ chainId: fromChain, network: currentNetwork })}{" "}
-                  {!!toChain &&
-                    ` to ${getChainName({ chainId: toChain, network: currentNetwork })}`}
-                </div>
+            <div className="progress-text">
+              <div className="progress-text-p">
+                Transfer {!isAttestation ? amountSent : ""}{" "}
+                {SOURCE_SYMBOL && (
+                  <>
+                    {SOURCE_SYMBOL} {amountSentUSD && `($${amountSentUSD}) `}
+                  </>
+                )}
+                {getChainName({ chainId: fromChain, network: currentNetwork })}{" "}
+                {!!toChain && ` to ${getChainName({ chainId: toChain, network: currentNetwork })}`}
               </div>
             </div>
-          )}
+          </div>
+        )}
 
         {status === "in_progress" && (
           <div className="progress-item wait">
@@ -177,21 +163,20 @@ const ProgressView = ({
 
             <div className="progress-text">
               <div className="progress-text-p">
-                {payloadType === 1 || payloadType === 3 ? "Transfer" : "Transaction"} completed
+                {payloadType !== 2 ? "Transfer" : "Transaction"} completed
               </div>
               <span>{destinationDateParsed}</span>
             </div>
           </div>
         )}
 
-        {status === "completed" && !targetTxHash && (payloadType === 1 || payloadType === 3) && (
+        {status === "completed" && !targetTxHash && payloadType !== 2 && (
           <div className="progress-item">
             <div className="progress-text">
               <div className="progress-text-p">
-                Message: Hey! This transaction doesn&apos;t have an associated Destination
-                Transaction. It may involve an application or process unknown to WormholeScan. Want
-                to add your protocol?{" "}
-                <NavLink to="/developers/submit">Submit Your Protocol!</NavLink>
+                We couldn&apos;t identify a Destination Transaction for this. It might involve an
+                unknown protocol (not integrated with WormholeScan) or be part of a multi-step
+                process.
               </div>
             </div>
           </div>
