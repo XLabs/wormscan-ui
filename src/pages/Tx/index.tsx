92da0c0a7da26136a211cffeacde4b7598ef0f8b
Giuliano Conti <giuliconti1@gmail.com>
2025-01-29 16:48:35 -0300

    GR + NTT details fixed (#930)
    * GR + NTT details fixed
    
    * check only ntt

--- src/pages/Tx/index.tsx
+++ src/pages/Tx/index.tsx
@@ -556,7 +556,11 @@ const Tx = () => {
 
         // Check if its generic relayer tx without vaa and go with RPCs
         // TODO: handle generic relayer no-vaa txns without RPCs
-        if (data?.content?.standarizedProperties?.appIds?.includes(GR_APP_ID) && !data?.vaa?.raw) {
+        if (
+          !data?.content?.standarizedProperties?.appIds?.includes(NTT_APP_ID) &&
+          data?.content?.standarizedProperties?.appIds?.includes(GR_APP_ID) &&
+          !data?.vaa?.raw
+        ) {
           setShouldTryToGetRpcInfo(true);
         }
         // ---
