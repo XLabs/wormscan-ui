751b6ce82d1fd8042860a8190d5de134fdfd236e
Valentino Conti <valeen.conti@hotmail.com>
2025-02-26 11:06:53 -0300

    Add Ink chain support

--- src/utils/filterUtils.tsx
+++ src/utils/filterUtils.tsx
@@ -81,6 +81,7 @@ export const ChainFilterMainnet = [
   chainToChainId("Ethereum"),
   chainToChainId("Fantom"),
   chainToChainId("Injective"),
+  // chainToChainId("Ink"),
   chainToChainId("Karura"),
   chainToChainId("Klaytn"),
   chainToChainId("Mantle"),
@@ -123,6 +124,7 @@ export const ChainFilterTestnet = [
   chainToChainId("Ethereum"), // Goerli
   chainToChainId("Holesky"),
   chainToChainId("Injective"),
+  // chainToChainId("Ink"),
   chainToChainId("Karura"),
   chainToChainId("Klaytn"),
   chainToChainId("Mantle"),
